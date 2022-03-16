6f38c52b6580495f62e78f3e5e1d60e3a8003043
KarenGil1 <82838111+KarenGil1@users.noreply.github.com>
2022-03-15 18:50:14 -0700

    ACTIVIDAD 10 SEMANA 5

---
+++ VisorImagen.java
@@ -0,0 +1,19 @@
+
+import java.io.Console;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vane_
+ */
+public class VisorImagen {
+    public static void main(String[] args) {
+        System.out.println("popo");
+        new NewJFrame().setVisible(true);
+    }
+    
+}
